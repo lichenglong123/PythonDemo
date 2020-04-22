51b07fef357dc7d65e428740ef50d34460396ff6
李成龙 <245767185@qq.com>
2020-04-22 17:09:33 +0800

    python项目提交

--- python/scikit_learn/mnist.py
+++ python/scikit_learn/mnist.py
@@ -13,20 +13,20 @@ some_digit=x[100]
 some_digit_image=some_digit.reshape(8,8)
 plt.imshow(some_digit_image,cmap=matplotlib.cm.binary,interpolation="nearest")
 plt.axis("off")
-#plt.show()
-shuffle_index=np.random.permutation(1500)
-x_train,y_train=x[shuffle_index],y[shuffle_index]
-
-y_train_5=(y_train==5)
-
-
-sgd_clf=SGDClassifier(random_state=42)
-
-sgd_clf.fit(x_train,y_train_5)
-
-
-
-print(sgd_clf.predict([some_digit]))
+plt.show()
+# shuffle_index=np.random.permutation(1500)
+# x_train,y_train=x[shuffle_index],y[shuffle_index]
+#
+# y_train_5=(y_train==5)
+#
+#
+# sgd_clf=SGDClassifier(random_state=42)
+#
+# sgd_clf.fit(x_train,y_train_5)
+#
+#
+#
+# print(sgd_clf.predict([some_digit]))
 
 
 
